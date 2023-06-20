232f06bc16fa0094574221f61d13390c4f6302a5
Ianwaswa <ianwaswa2019@gmail.com>
2023-06-20 00:52:04 -0700

    task 0

---
+++ practice_day2/day3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point to my morning check out project
+ * project purposed for while nested loops
+ * return: always 0
+ */
+int main(void)
+{
+	int n = 0;
+	while (i < 5) {
+		printf("%d\n", i);
+		i++
+	}
+	return 0;
+}
